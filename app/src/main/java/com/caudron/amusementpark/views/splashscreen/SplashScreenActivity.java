343f7a2d8929204c28a4ba2d965e2f854397116a
ccaud <c.caudron59@gmail.com>
2023-03-29 19:52:34 +0200

    TEST voir si le nombre de resultat est supérieur après enregistrement en base

--- app/src/main/java/com/caudron/amusementpark/views/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/caudron/amusementpark/views/splashscreen/SplashScreenActivity.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -230,9 +231,14 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
     private void launchMainActivity() {
-        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 10000); // 10000 ms = 10s
     }
 
     private void showErrorToast() {
